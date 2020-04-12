97398e6d1bcd8c44fa2c7edde86c8a921413b692
Jonathan Miranda <miranda.jth@gmail.com>
2020-04-12 15:44:04 -0600

    Character Is a Number

--- swfit-5-new-features.playground/Pages/Character is a Number.xcplaygroundpage/Contents.swift
+++ swfit-5-new-features.playground/Pages/Character is a Number.xcplaygroundpage/Contents.swift
@@ -1,7 +1,28 @@
 //: [Previous](@previous)
 
-import Foundation
+func characterIsNumber(string: String) {
+    
+    let characters = Array(string)
+    
+    for character in characters {
+        if character.isNumber {
+            print("\(character) is a number")
+        } else {
+            print("\(character) is NOT a number")
+        }
+        
+//        let stringCharacter = String(character)
+//        let characterInt = Int(stringCharacter)
+//
+//        if characterInt != nil {
+//            print("\(character) is a number")
+//        } else {
+//            print("\(character) is NOT a number")
+//        }
+        
+    }
+}
 
-var str = "Hello, playground"
+characterIsNumber(string: "A8273UJD79J3HD")
 
 //: [Next](@next)

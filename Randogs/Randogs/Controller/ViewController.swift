52a4e74bc7c9764243564100acbc3c19f0656c62
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-08 11:18:59 -0600

    setting view and controller to show an image from query the endpoint

--- Randogs/Randogs/Controller/ViewController.swift
+++ Randogs/Randogs/Controller/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        DogAPI.requestRandomImage(completionHandler: self.handleRandomImageResponse(dogImage:error:))
     }
 
+    
+    func handleRandomImageResponse(dogImage: DogImage?, error: Error?) {
+        if let dogImage = dogImage {
+            guard let imageUrl = URL(string: dogImage.message) else {
+                return
+            }
+            DogAPI.requestImageFile(url: imageUrl, completionHandler: self.handleImageFileResponse(image:error:))
+        }
+    }
 
+    func handleImageFileResponse(image: UIImage?, error: Error?) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+    
 }
 
---
+++ Randogs/Randogs/Model/DogImage.swift
@@ -0,0 +1,15 @@
+//
+//  DogImage.swift
+//  Randogs
+//
+//  Created by Jonathan Miranda on 5/8/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import Foundation
+
+
+struct DogImage: Codable {
+    let status: String
+    let message: String
+}

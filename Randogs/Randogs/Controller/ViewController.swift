b190f877041a54576bf325846541f63a71e28cf6
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-08 12:26:22 -0600

    applying new parameter to the method invocation from controller

--- Randogs/Randogs/Controller/ViewController.swift
+++ Randogs/Randogs/Controller/ViewController.swift
@@ -54,7 +54,7 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        DogAPI.requestRandomImage(completionHandler: self.handleRandomImageResponse(dogImage:error:))
+        DogAPI.requestRandomImage(breed: breeds[row], completionHandler: self.handleRandomImageResponse(dogImage:error:))
     }
     
 }

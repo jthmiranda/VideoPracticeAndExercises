8a85990fe812f215a8a4d55350c9f7f12ad6a234
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-24 15:27:49 -0600

    creating a background context and defining Merge Policy

--- Mooskine/Mooskine/Model/DataController.swift
+++ Mooskine/Mooskine/Model/DataController.swift
@@ -16,8 +16,20 @@ class DataController {
         return persistentContainer.viewContext
     }
     
+    var backgroundContext: NSManagedObjectContext!
+    
     init(modelName: String) {
         persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func configureContexts() {
+        backgroundContext = persistentContainer.newBackgroundContext()
+        
+        viewContext.automaticallyMergesChangesFromParent = true
+        backgroundContext.automaticallyMergesChangesFromParent = true
+        
+        backgroundContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
     }
     
     func load(completion: (() -> Void)? = nil) {
@@ -26,6 +38,7 @@ class DataController {
                 fatalError()
             }
             self.autoSaveViewContext()
+            self.configureContexts()
             completion?()
         }
     }

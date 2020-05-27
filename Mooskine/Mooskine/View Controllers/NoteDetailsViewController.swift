7ed10b7d8a1607bf5926e80edb7beba826b7db38
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-26 21:02:10 -0600

    removing comments

--- Mooskine/Mooskine/View Controllers/NoteDetailsViewController.swift
+++ Mooskine/Mooskine/View Controllers/NoteDetailsViewController.swift
@@ -170,11 +170,6 @@ extension NoteDetailsViewController {
             backgrountNote.attributedText = newText
             try? backgroundContext.save()
         }
-        
-        
-        
-//        textView.attributedText = newText
-//        textView.selectedRange = NSMakeRange(selectedRange.location, 1)
     }
 
     // MARK: Helper methods for actions

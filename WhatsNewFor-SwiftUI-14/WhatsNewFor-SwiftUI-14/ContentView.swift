07192f757b6b516f94eafa609942134e888d5bc3
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-26 12:23:17 -0600

    ColorPicker example

--- WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
+++ WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
@@ -6,37 +6,13 @@
 //
 
 import SwiftUI
-import SpriteKit
 
-class GameScene: SKScene {
-    override func didMove(to view: SKView) {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {
-            return
-        }
-        let location = touch.location(in: self)
-        
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
-        box.position = location
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
-        addChild(box)
-    }
-}
 
 struct ContentView: View {
-    var scene: SKScene {
-        let scene = GameScene()
-        scene.size = CGSize(width: 300, height: 400)
-        scene.scaleMode = .fill
-        return scene
-    }
+    @State private var bgColor = Color.white
     
     var body: some View {
-        SpriteView(scene: scene)
-            .frame(width: 300, height: 400)
+        ColorPicker("Set the background color", selection: $bgColor)
     }
 }
 

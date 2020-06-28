619caf5361d59eb1f78ca7a5568ba79ba65d8a54
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-28 17:05:45 -0600

    How to open web links in Safari - example

--- WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
+++ WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.openURL) var openURL
 
     var body: some View {
-        // This is buggy a this point beta 1 Xcode 12
-       //Label("Jonathan Miranda", systemImage: "person.circle")
-        
-        
-        Label {
-            Text("Jonathan Miranda")
-                .foregroundColor(.primary)
-                .font(.title)
-                .padding()
-                .background(Color.gray.opacity(0.2))
-                .clipShape(Capsule())
-        } icon: {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.blue)
-                .frame(width: 64, height: 64)
+        VStack {
+            Link(destination: URL(string: "https://www.udacity.com")!) {
+                Text("Visit Apple")
+            }
+            .padding(.all, 10)
+            
+            Button("another link") {
+                openURL(URL(string: "http://www.apple.com")!)
+            }
+            .padding(.all, 10)
+            .background(Color.gray.opacity(0.2))
+            .clipShape(Capsule())
         }
     }
 }

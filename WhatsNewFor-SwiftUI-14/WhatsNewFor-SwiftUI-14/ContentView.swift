ca1eb861eb729da60a06f34df49f6aba74ea328f
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-29 20:43:15 -0600

    How to add a sidebar for iPadOS

--- WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
+++ WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
@@ -7,35 +7,37 @@
 
 import SwiftUI
 
-struct Bookmark: Identifiable {
-    var id = UUID()
-    var name: String
-    var icon: String
-    var items: [Bookmark]?
-}
-
-extension Bookmark {
+struct SideBar: View {
     
-    // some example websites
-        static let apple = Bookmark(name: "Apple", icon: "1.circle")
-        static let bbc = Bookmark(name: "BBC", icon: "square.and.pencil")
-        static let swift = Bookmark(name: "Swift", icon: "bolt.fill")
-        static let twitter = Bookmark(name: "Twitter", icon: "mic")
-
-        // some example groups
-        static let example1 = Bookmark(name: "Favorites", icon: "star", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
-        static let example2 = Bookmark(name: "Recent", icon: "timer", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
-        static let example3 = Bookmark(name: "Recommended", icon: "hand.thumbsup", items: [Bookmark.apple, Bookmark.bbc, Bookmark.swift, Bookmark.twitter])
+    var body: some View {
+        List(1..<100) { i in
+            Text("Row \(i)")
+        }
+        .listStyle(SidebarListStyle())
+    }
 }
 
+struct PrimaryView: View {
+    
+    var body: some View {
+        Text("Primary view")
+    }
+}
+
+struct DetailView: View {
+    
+    var body: some View {
+        Text("Detail view")
+    }
+}
 struct ContentView: View {
-    let items: [Bookmark] = [Bookmark.example1, Bookmark.example2, Bookmark.example3]
+   
 
     var body: some View {
-        
-        List(items, children: \.items) { row in
-            Image(systemName: row.icon)
-            Text(row.name)
+        NavigationView {
+            SideBar()
+            PrimaryView()
+            DetailView()
         }
     }
 }

1058658c92a56c7cbc3713b3fb9eb01b62cb041a
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-29 22:04:40 -0600

    How to synchronize animations from one view to another with matchedGeometryEffect() - Advanced

--- WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
+++ WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
@@ -10,33 +10,50 @@ import SwiftUI
 
 struct ContentView: View {
     @Namespace private var animation
-    @State private var isFlipped = false
+    @State private var isZoomed = false
+    
+    var frame: CGFloat {
+        isZoomed ? 300 : 44
+    }
     
     var body: some View {
         VStack {
-            if isFlipped {
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 44, height: 44)
-                    .matchedGeometryEffect(id: "Shape", in: animation)
-                Text("Taylor Swift - 1989")
-                    .font(.headline)
-                    .matchedGeometryEffect(id: "Album", in: animation)
-            } else {
-                Text("Taylor Swift - 1989")
-                    .font(.headline)
-                    .matchedGeometryEffect(id: "Album", in: animation)
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 44, height: 44)
-                    .matchedGeometryEffect(id: "Shape", in: animation)
+            Spacer()
+            
+            VStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                        .frame(width: frame, height: frame)
+                        .padding(.top, isZoomed ? 20 : 0)
+                    
+                    if isZoomed == false {
+                        Text("Taylor Swift - 1989")
+                            .matchedGeometryEffect(id: "Album", in: animation)
+                            .font(.headline)
+                        Spacer()
+                    }
+                }
+                
+                if isZoomed == true {
+                    Text("Taylor Swift - 1989")
+                        .matchedGeometryEffect(id: "Album", in: animation)
+                        .font(.headline)
+                        .padding(.bottom, 60)
+                    Spacer()
+                }
             }
-        }
-        .onTapGesture {
-            withAnimation {
-                isFlipped.toggle()
+            .onTapGesture {
+                withAnimation {
+                    isZoomed.toggle()
+                }
             }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(height: isZoomed ? 400 : 60)
+            .background(Color(white: 0.9))
         }
+        
     }
 }
 

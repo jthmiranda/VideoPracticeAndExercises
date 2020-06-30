c3f0a9c312a2497088c75c5aa3b8a94bb5eb65ad
Jonathan Miranda <miranda.jth@gmail.com>
2020-06-29 23:09:39 -0600

    How to make a scroll view move to a location using ScrollViewReader

--- WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
+++ WhatsNewFor-SwiftUI-14/WhatsNewFor-SwiftUI-14/ContentView.swift
@@ -9,51 +9,25 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @Namespace private var animation
-    @State private var isZoomed = false
-    
-    var frame: CGFloat {
-        isZoomed ? 300 : 44
-    }
+    let colors: [Color] = [.red, .green, .blue]
     
     var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack {
-                HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.blue)
-                        .frame(width: frame, height: frame)
-                        .padding(.top, isZoomed ? 20 : 0)
-                    
-                    if isZoomed == false {
-                        Text("Taylor Swift - 1989")
-                            .matchedGeometryEffect(id: "Album", in: animation)
-                            .font(.headline)
-                        Spacer()
+        ScrollView {
+            ScrollViewReader { reader in
+                Button("scroll to 8") {
+                    withAnimation {
+                        reader.scrollTo(8, anchor: .center)
                     }
                 }
                 
-                if isZoomed == true {
-                    Text("Taylor Swift - 1989")
-                        .matchedGeometryEffect(id: "Album", in: animation)
-                        .font(.headline)
-                        .padding(.bottom, 60)
-                    Spacer()
+                ForEach(1..<10, id:\.self) { i in
+                    Text("Row \(i)")
+                        .frame(width: 300, height: 300)
+                        .background(colors[i % colors.count])
+                        .id(i)
                 }
             }
-            .onTapGesture {
-                withAnimation {
-                    isZoomed.toggle()
-                }
-            }
-            .padding()
-            .frame(maxWidth: .infinity)
-            .frame(height: isZoomed ? 400 : 60)
-            .background(Color(white: 0.9))
         }
-        
     }
 }
 
